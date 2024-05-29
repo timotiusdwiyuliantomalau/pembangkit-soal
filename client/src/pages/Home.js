243c3d933226baf22c7cec3761b366e3f78bbff0
timotiusdwiyuliantomalau <162861450+timotiusdwiyuliantomalau@users.noreply.github.com>
2024-05-30 02:00:25 +0700

    Update Home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -6,7 +6,7 @@ const [inputSoal,setInputSoal]=useState('');
   const [hasilSoal,setHasilSoal]=useState([]);
   const [jumlahSoal,setJumlahSoal]=useState([]);
   const [nomor,setNomor]=useState(0);
-
+axios.defaults.withCredentials=true;
   async function handleDataSoal(e){
   document.body.style.cursor='wait';
   setTimeout(()=>{
