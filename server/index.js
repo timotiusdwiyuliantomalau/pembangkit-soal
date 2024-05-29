ef2053b7e72b0af01929090d520176edb5cd55ff
timotiusdwiyuliantomalau <162861450+timotiusdwiyuliantomalau@users.noreply.github.com>
2024-05-30 01:37:13 +0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -16,8 +16,6 @@ app.use(cors({
   origin:["https://pembangkit-soal-apps.vercel.app"],
   methods:["POST","GET"],
   credentials:true,
-  preflightContinue: false,
-  allowedHeaders: ['Content-Type'],
 }));
 
 const port = 3001;
