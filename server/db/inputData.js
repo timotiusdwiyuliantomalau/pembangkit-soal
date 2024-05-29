34b3cfc4d29a62e1f74d846b86c61b0f4210ae53
timotiusdwiyuliantomalau <timotiusdym17@gmail.com>
2024-05-30 01:07:37 +0700

    update

--- server/db/inputData.js
+++ server/db/inputData.js
@@ -1,17 +1,7 @@
 import mongoose from "mongoose";
 import express from "express";
-import http from "http";
 const app = express();
-const server = http.createServer(app);
 const connect = () => {
-mongoose.connect("mongodb+srv://pembangkit-soal:123@cluster0.8i5kff4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(() => {
-  console.log("Mongodb connected");
-  server.listen(3003, () => {
-    console.log(`Server is listening on port 3001`);
-  });
-}).catch((err) => {
-  console.log({ err });
-  process.exit(1);
-});
+mongoose.connect("mongodb+srv://pembangkit-soal:123@cluster0.8i5kff4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 };
 export { connect };
