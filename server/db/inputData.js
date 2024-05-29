e01c918cdb4bb56e1a5469ac94fd043c3c536ab2
timotiusdwiyuliantomalau <timotiusdym17@gmail.com>
2024-05-29 22:12:33 +0700

    update

--- server/db/inputData.js
+++ server/db/inputData.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 const connect = () => {
-  mongoose.connect("mongodb+srv://pembangkit-soal:<password>@cluster0.8i5kff4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+  mongoose.connect("mongodb+srv://pembangkit-soal:123@cluster0.8i5kff4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
   
 
 
